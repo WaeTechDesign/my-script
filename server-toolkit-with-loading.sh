1f5ca3ee6987da93723fd80e0cd63e6794e633ca
WaeTechDesign <hackwae3@gmail.com>
2025-01-26 20:12:07 +0800

    Home Server WAE TECH DESIGN Script

--- server-toolkit-with-loading.sh
+++ server-toolkit-with-loading.sh
@@ -17,6 +17,20 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
+spinner() {
+    local pid=$!
+    local delay=0.1
+    local spinstr='|/-\'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
+
 # Function to print header
 print_header() {
     echo "================================================"
